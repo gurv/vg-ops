372f9c5934dd410f8d13e910d71132338e29f27e
gurv <vladimir.gurinovich@gmail.com>
2018-10-07 20:46:51 +0300

    fix (build): Success start on Docker Swarm (& dos2unix)

--- docker-cli/src/main/docker/docker-entrypoint.sh
+++ docker-cli/src/main/docker/docker-entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -ex
 
-#TODO адрес Docker-хоста
-#export DOCKER_HOST='tcp://docker:2375' # что здесь должно быть?
-# под Linux внутри контейнера можно так определить адрес Docker-хоста
-#   route -n | awk '/UG[ \t]/{print $2}'
-
 if [ "$1" = 'docker' ]; then
     shift
     /opt/docker "$@"
